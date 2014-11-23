0ef359a54ea4bc3d29a66a11752799310d55787d
JoeSelvik <joe@thenewtricks.com>
2014-11-22 18:07:12 -0700

    trying to get uilabel frames to animate

--- uiv4Skeleton/ViewController.m
+++ uiv4Skeleton/ViewController.m
@@ -12,8 +12,12 @@
 
 @property (weak, nonatomic) IBOutlet UIView *locationBarView;
 @property (weak, nonatomic) IBOutlet UIView *pickupBarView;
+@property (weak, nonatomic) IBOutlet UILabel *pickupBarLabel;
 @property (weak, nonatomic) IBOutlet UIView *dropoffBarView;
+@property (weak, nonatomic) IBOutlet UILabel *dropoffBarLabel;
 @property (weak, nonatomic) IBOutlet UIButton *locationButton;
+
+@property (nonatomic, assign) BOOL pickupBarSelected;
 
 @end
 
@@ -32,6 +36,11 @@
     [self.dropoffBarView addGestureRecognizer:tapDropoffBarView];
     [self.pickupBarView addGestureRecognizer:tapPickupBarView];
     
+    self.pickupBarLabel.contentMode = UIViewContentModeScaleToFill;
+    self.dropoffBarLabel.contentMode = UIViewContentModeScaleToFill;
+    
+    self.pickupBarSelected = YES;
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,36 +51,64 @@
 
 - (void)tapRight:(UITapGestureRecognizer *)tapRecognizer
 {
-    CGFloat screenWidth = self.locationBarView.frame.size.width;
-    CGFloat pickupBarViewWidth = screenWidth * 0.25;
-    CGFloat dropoffBarViewWidth = screenWidth * 0.75;
+    if (self.pickupBarSelected) {
+        CGFloat screenWidth = self.locationBarView.frame.size.width;
+        CGFloat pickupBarViewWidth = screenWidth * 0.40;
+        CGFloat dropoffBarViewWidth = screenWidth * 0.60;
+        
+        CGRect newPickupBarLabelFrame = self.dropoffBarLabel.frame;
+        CGRect newDropoffBarLabelFrame = self.pickupBarLabel.frame;
+        NSLog(@"pre - pickupBarLabel frame: %@", NSStringFromCGRect(self.pickupBarLabel.frame));
+        NSLog(@"pre - dropoffBarLabel frame: %@", NSStringFromCGRect(self.dropoffBarLabel.frame));
+        
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveLinear
+                         animations:^{
+                             self.pickupBarView.frame = CGRectMake(0, 0, pickupBarViewWidth, 80);
+                             self.dropoffBarView.frame = CGRectMake(pickupBarViewWidth, 0, dropoffBarViewWidth, 80);
+                             
+                             self.pickupBarLabel.frame = newPickupBarLabelFrame;
+                             self.dropoffBarLabel.frame = newDropoffBarLabelFrame;
+                         }
+                         completion:nil
+         ];
+        
+        NSLog(@"post - pickupBarLabel frame: %@", NSStringFromCGRect(self.pickupBarLabel.frame));
+        NSLog(@"post - dropoffBarLabel frame: %@", NSStringFromCGRect(self.dropoffBarLabel.frame));
+        self.pickupBarSelected = NO;
+    }
     
-    [UIView animateWithDuration:1.0
-                          delay:0.0
-                        options:UIViewAnimationOptionCurveLinear
-                     animations:^{
-                         self.pickupBarView.frame = CGRectMake(0, 0, pickupBarViewWidth, 80);
-                         self.dropoffBarView.frame = CGRectMake(pickupBarViewWidth, 0, dropoffBarViewWidth, 80);
-                     }
-                     completion:nil
-     ];
 }
 
 - (void)tapLeft:(UITapGestureRecognizer *)tapRecognizer
 {
-    CGFloat screenWidth = self.locationBarView.frame.size.width;
-    CGFloat pickupBarViewWidth = screenWidth * 0.75;
-    CGFloat dropoffBarViewWidth = screenWidth * 0.25;
+    if (!self.pickupBarSelected) {
+        CGFloat screenWidth = self.locationBarView.frame.size.width;
+        CGFloat pickupBarViewWidth = screenWidth * 0.60;
+        CGFloat dropoffBarViewWidth = screenWidth * 0.40;
+        
+        CGRect newPickupBarLabelFrame = self.dropoffBarLabel.frame;
+        CGRect newDropoffBarLabelFrame = self.pickupBarLabel.frame;
+        
+        [UIView animateWithDuration:0.5
+                              delay:0.0
+                            options:UIViewAnimationOptionCurveLinear
+                         animations:^{
+                             self.pickupBarView.frame = CGRectMake(0, 0, pickupBarViewWidth, 80);
+                             self.dropoffBarView.frame = CGRectMake(pickupBarViewWidth, 0, dropoffBarViewWidth, 80);
+                             
+                             self.pickupBarLabel.frame = newPickupBarLabelFrame;
+                             self.dropoffBarLabel.frame = newDropoffBarLabelFrame;
+                         }
+                         completion:nil
+         ];
+        
+        NSLog(@"pickupBarLabel frame: %@", NSStringFromCGRect(self.pickupBarLabel.frame));
+        NSLog(@"dropoffBarLabel frame: %@", NSStringFromCGRect(self.dropoffBarLabel.frame));
+        self.pickupBarSelected = YES;
+    }
     
-    [UIView animateWithDuration:1.0
-                          delay:0.0
-                        options:UIViewAnimationOptionCurveLinear
-                     animations:^{
-                         self.pickupBarView.frame = CGRectMake(0, 0, pickupBarViewWidth, 80);
-                         self.dropoffBarView.frame = CGRectMake(pickupBarViewWidth, 0, dropoffBarViewWidth, 80);
-                     }
-                     completion:nil
-     ];
 }
 
 @end
