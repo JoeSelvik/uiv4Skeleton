1a73411decdf0ef97aecc7905a37aaae9cd4f1a7
JoeSelvik <joe@thenewtricks.com>
2014-12-01 23:11:32 -0700

    cleaned up the new map VC file

--- uiv4Skeleton/MapViewController.m
+++ uiv4Skeleton/MapViewController.m
@@ -22,8 +22,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    //[self loadBottomContainer];
+
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,47 +36,6 @@
     
 }
 
-- (void)loadBottomContainer
-{
-    // Grab the locationBar and button
-    UIStoryboard *main = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    LocationBarViewController *locationBarVC = [main instantiateViewControllerWithIdentifier:@"locationBarVC"];
-    locationBarVC.view.translatesAutoresizingMaskIntoConstraints = NO;
-    self.locationBarVC = locationBarVC;
-    
-    //[self.bottomContainer addSubview:locationBarVC.view];
-    NSDictionary *viewsDictionary = @{@"locationBarView":self.locationBarVC.view};
-    
-    // Leading constraint
-    NSArray *constraint_LEADING = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[locationBarView]"
-                                                                          options:0
-                                                                          metrics:nil
-                                                                            views:viewsDictionary];
-    [self.bottomContainer addConstraints:constraint_LEADING];
-    
-    // Top constraint
-    NSArray *constraint_TOP = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[locationBarView]"
-                                                                        options:0
-                                                                        metrics:nil
-                                                                        views:viewsDictionary];
-    [self.bottomContainer addConstraints:constraint_TOP];
-    
-    // Trailing constraint
-    NSArray *constraint_TRAILING = [NSLayoutConstraint constraintsWithVisualFormat:@"H:[locationBarView]-0-|"
-                                                                           options:0
-                                                                           metrics:nil
-                                                                             views:viewsDictionary];
-    [self.bottomContainer addConstraints:constraint_TRAILING];
-    
-    // Height constraint
-    NSArray *constraint_HEIGHT = [NSLayoutConstraint constraintsWithVisualFormat:@"V:[locationBarView(80)]"
-                                                                         options:0
-                                                                         metrics:nil
-                                                                           views:viewsDictionary];
-    [self.locationBarVC.view addConstraints:constraint_HEIGHT];
-    
-    
-}
 
 
 @end
